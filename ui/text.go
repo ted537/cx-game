24f906bb382d96cfbba7ce480f01894d269e79a9
Ted Brownlow <ted.brownlow@gmail.com>
2021-05-10 16:29:31 -0400

    begin applying single VBO for font

--- ui/text.go
+++ ui/text.go
@@ -1,4 +1,7 @@
 package ui;
+// runtime text rendering library.
+// uses a single VAO/VBO pair
+// and draws a different set of triangles depending on the character.
 
 import (
 	"log"
@@ -10,7 +13,81 @@ import (
 )
 
 var asciiToCharDataMap = make(map[int]CharData)
-var fontTex uint32
+// opengl objects
+var fontTex,vao,vbo uint32
+
+// allocate a VBO for the entire font which can 
+// render different characters with very little computation
+func initFontVbo() {
+	var vertexAttributes = make([]float32,5*6*len(charDatas))
+	i := 0
+	for _,charData := range charDatas {
+		top := float32(charData.ty + charData.h)/256
+		bottom := float32(charData.ty)/256
+		right := float32(charData.tx + charData.w)/256
+		left := float32(charData.tx)/256
+
+		vertexAttributes[i] = right
+		vertexAttributes[i+1] = top
+		vertexAttributes[i+2] = 0
+		vertexAttributes[i+3] = 1
+		vertexAttributes[i+4] = 0
+		i += 5
+
+		vertexAttributes[i] = right
+		vertexAttributes[i+1] = bottom
+		vertexAttributes[i+2] = 0
+		vertexAttributes[i+3] = 1
+		vertexAttributes[i+4] = 1
+		i += 5
+
+		vertexAttributes[i] = left
+		vertexAttributes[i+1] = top
+		vertexAttributes[i+2] = 0
+		vertexAttributes[i+3] = 1
+		vertexAttributes[i+4] = 1
+		i += 5
+
+		vertexAttributes[i] = right
+		vertexAttributes[i+1] = bottom
+		vertexAttributes[i+2] = 0
+		vertexAttributes[i+3] = 1
+		vertexAttributes[i+4] = 1
+		i += 5
+
+		vertexAttributes[i] = left
+		vertexAttributes[i+1] = bottom
+		vertexAttributes[i+2] = 0
+		vertexAttributes[i+3] = 1
+		vertexAttributes[i+4] = 1
+		i += 5
+
+		vertexAttributes[i] = left
+		vertexAttributes[i+1] = top
+		vertexAttributes[i+2] = 0
+		vertexAttributes[i+3] = 1
+		vertexAttributes[i+4] = 1
+		i += 5
+	}
+	log.Print(vertexAttributes[6*5:6*5+6*5])
+
+	gl.GenBuffers(1,&vbo)
+	gl.GenVertexArrays(1,&vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER,vbo)
+	gl.BufferData(
+		gl.ARRAY_BUFFER,
+		4*len(vertexAttributes),
+		gl.Ptr(vertexAttributes),
+		gl.STATIC_DRAW,
+	)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(4*3))
+	gl.EnableVertexAttribArray(1)
+}
+
 func InitTextRendering() {
 	_,img,_ := spriteloader.LoadPng("assets/font/8bitoperator_jve.png")
 	fontTex = spriteloader.MakeTexture(img)
@@ -18,6 +95,8 @@ func InitTextRendering() {
 	for _,charData := range charDatas {
 		asciiToCharDataMap[charData.ascii] = charData
 	}
+
+	initFontVbo()
 }
 
 func calculateLineWidth(text string) float32 {
@@ -60,19 +139,6 @@ func DrawString(text string, transform mgl32.Mat4) {
 			_ = letterTransform
 			_ = charData
 
-			texScale := float32(charData.h) / 256
-			//texScale := float32(1/math.Max(float64(charData.w),float64(charData.h)))
-			gl.Uniform2f(
-				gl.GetUniformLocation(program, gl.Str("texScale\x00")),
-				texScale, texScale,
-			)
-			// FIXME
-			gl.Uniform2f(
-				gl.GetUniformLocation(program, gl.Str("texOffset\x00")),
-				float32(charData.tx)/256/texScale, float32(charData.ty)/256/texScale,
-			)
-			log.Printf("drawing character %v with u %v",string(charData.ascii), float32(charData.tx)/256)
-
 			gl.UniformMatrix4fv(
 				gl.GetUniformLocation(program, gl.Str("world\x00")),
 				1, false, &letterTransform[0],
@@ -85,21 +151,11 @@ func DrawString(text string, transform mgl32.Mat4) {
 				gl.GetUniformLocation(program, gl.Str("projection\x00")),
 				1, false, &projectTransform[0],
 			)
-			gl.BindVertexArray(spriteloader.QuadVao)
-			gl.DrawArrays(gl.TRIANGLES, 0, 6)
-			// restore texScale and texOffset to defaults
-			// TODO separate GPU programs such that this becomes unecessary
-			gl.Uniform2f(
-				gl.GetUniformLocation(program, gl.Str("texScale\x00")),
-				1, 1,
-			)
-			gl.Uniform2f(
-				gl.GetUniformLocation(program, gl.Str("texOffset\x00")),
-				0, 0,
-			)
+			gl.BindVertexArray(vao)
+			glStart := 6*charData.index
+			gl.DrawArrays(gl.TRIANGLES, int32(glStart), 6)
 		}
 		// TODO variable width fonts
 		pos = pos.Add(mgl32.Vec2{1.1,0})
-		log.Print(pos)
 	}
 }
