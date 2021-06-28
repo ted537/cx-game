13bb3064173942d47fd120933f7c3836fb32a9b7
Ted Brownlow <ted.brownlow@gmail.com>
2021-06-28 13:07:11 -0400

    compute inner corners to find blob coord

--- render/blob.go
+++ render/blob.go
@@ -26,6 +26,22 @@ func (n Neighbours) downRightInnerCorner() bool {
 	return n.Down && n.Right && !n.DownRight
 }
 
+func (n Neighbours) hasLeftInnerCorner() bool {
+	return n.upLeftInnerCorner() || n.downLeftInnerCorner()
+}
+
+func (n Neighbours) hasRightInnerCorner() bool {
+	return n.upRightInnerCorner() || n.downRightInnerCorner()
+}
+
+func (n Neighbours) hasUpInnerCorner() bool {
+	return n.upLeftInnerCorner() || n.upRightInnerCorner()
+}
+
+func (n Neighbours) hasDownInnerCorner() bool {
+	return n.downLeftInnerCorner() || n.downRightInnerCorner()
+}
+
 func (n Neighbours) countInnerCorners() int {
 	return boolToInt(n.upLeftInnerCorner()) +
 		boolToInt(n.upRightInnerCorner()) +
@@ -33,17 +49,39 @@ func (n Neighbours) countInnerCorners() int {
 		boolToInt(n.downRightInnerCorner())
 }
 
+func (n Neighbours) countEdges() int {
+	return boolToInt(n.Left) + boolToInt(n.Up) +
+		boolToInt(n.Right) + boolToInt(n.Down)
+}
+
 func boolToInt(x bool) int {
 	if x { return 0 } else { return 1 }
 }
 
-func (n Neighbours) blobCoords() (int,int) {
+// compute coords on the maps TileSetter provides in their "Blob" format
+func (n Neighbours) blobCoords() (x,y int) {
 	innerCorners := n.countInnerCorners()
+	// default to the solid square
+	x = 1
+	y = 1
 	if innerCorners == 0 {
+		x = 1 + boolToInt(n.Up) - boolToInt(n.Down)
+		y = 1 + boolToInt(n.Up) - boolToInt(n.Down)
+		if !n.Left && !n.Right { x = 3 }
+		if !n.Up && !n.Down { y = 3 }
 
+		return x,y
 	}
 	if innerCorners == 1 {
+		if !n.Left { x = 4 }
+		if n.hasRightInnerCorner() { x = 5 }
+		if n.hasLeftInnerCorner() { x = 6 }
+		if !n.Right { x = 7 }
 
+		if !n.Up { y = 0 }
+		if !n.hasDownInnerCorner() { y = 1 }
+		if !n.hasUpInnerCorner() { y = 2 }
+		if !n.Down { y = 3 }
 	}
 	if innerCorners == 2 {
 
@@ -54,7 +92,7 @@ func (n Neighbours) blobCoords() (int,int) {
 	if innerCorners == 4 {
 
 	}
-	return -1,-1
+	return x,y
 }
 
 func DrawBlobTile(mvp mgl32.Mat4, neighbours Neighbours) {
