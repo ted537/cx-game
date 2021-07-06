e88fd5b16f4564aa92314d42b2cbf6c3e4490571
Ted Brownlow <ted.brownlow@gmail.com>
2021-07-06 17:22:11 -0400

    bin tiletypes by material

--- ui/inventory-grid.go
+++ ui/inventory-grid.go
@@ -1,4 +1,29 @@
 package ui
 
+import (
+	"github.com/skycoin/cx-game/world"
+	"github.com/skycoin/cx-game/cxmath"
+)
+
 const InventoryGridWidth = 5
 
+func binTileTypesByMaterial(
+		tiletypes []world.TileType,
+) map[world.MaterialID][]world.TileType {
+	bins := make(map[world.MaterialID][]world.TileType)
+	for _,tiletype := range tiletypes {
+		_,ok := bins[tiletype.MaterialID]
+		if !ok { bins[tiletype.MaterialID] = []world.TileType{} }
+		bins[tiletype.MaterialID] =
+			append(bins[tiletype.MaterialID], tiletype)
+	}
+	return bins
+}
+
+func LayoutMaterials(tiletypes []world.TileType) []cxmath.Vec2i {
+	bins := binTileTypesByMaterial(tiletypes)
+	_ = bins
+	positions := []cxmath.Vec2i{}
+	
+	return positions
+}
