7ca101c2a568565c4413cf325de3af1549b7653e
Ted Brownlow <ted.brownlow@gmail.com>
2021-08-05 10:27:19 -0400

    player agent can be controlled

--- components/agents/agent_ai/player.go
+++ components/agents/agent_ai/player.go
@@ -2,8 +2,24 @@ package agent_ai
 
 import (
 	"github.com/skycoin/cx-game/agents"
+	"github.com/skycoin/cx-game/input"
+	"github.com/skycoin/cx-game/cxmath"
+	"github.com/skycoin/cx-game/cxmath/math32"
 )
 
-func AiHandlerPlayer(agent *agents.Agent, ctx AiContext) {
+const playerWalkSpeed float32 = 5
+const frictionFactor float32 = 3
+
+func AiHandlerPlayer(player *agents.Agent, ctx AiContext) {
 	// TODO
+	inputXAxis := input.GetAxis(input.HORIZONTAL)
+	player.PhysicsState.Vel.X +=
+		inputXAxis * playerWalkSpeed
+
+	friction :=
+		cxmath.Sign(player.PhysicsState.Vel.X) * frictionFactor
+
+	if math32.Abs(friction) < math32.Abs(player.PhysicsState.Vel.X) {
+		player.PhysicsState.Vel.X -= friction
+	}
 }
