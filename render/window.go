ce8dc5275731dc844966f23fdd90b0227009a398
Ted Brownlow <ted.brownlow@gmail.com>
2021-08-11 15:18:56 -0400

    remove unused code

--- render/window.go
+++ render/window.go
@@ -76,52 +76,6 @@ func initOpenGL() {
 		"./assets/shader/line.vert", "./assets/shader/line.frag")
 }
 
-/*
-func CreateProgram(vertexShaderSource string, fragmentShaderSource string) uint32 {
-	vertexShader, err := CompileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	fragmentShader, err := CompileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	prog := gl.CreateProgram()
-	gl.AttachShader(prog, vertexShader)
-	gl.AttachShader(prog, fragmentShader)
-	gl.LinkProgram(prog)
-	gl.DeleteShader(fragmentShader)
-	gl.DeleteShader(vertexShader)
-
-	return prog
-}
-
-func CompileShader(source string, shaderType uint32) (uint32, error) {
-	shader := gl.CreateShader(shaderType)
-
-	csources, free := gl.Strs(source)
-	gl.ShaderSource(shader, 1, csources, nil)
-	free()
-	gl.CompileShader(shader)
-
-	var status int32
-	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
-	}
-
-	return shader, nil
-}
-*/
-
 var Projection mgl32.Mat4
 
 func (window *Window) GetProjectionMatrix() mgl32.Mat4 {
