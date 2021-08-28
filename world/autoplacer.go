e8b3363fa5b7f0d63fa454eef01283421c365390
Ted Brownlow <ted.brownlow@gmail.com>
2021-08-28 10:20:12 -0400

    refactor autoplacer

--- world/autoplacer.go
+++ world/autoplacer.go
@@ -29,25 +29,26 @@ func (placer AutoPlacer) sprite(
 func (placer AutoPlacer) CreateTile(
 	tt TileType, createOpts TileCreationOptions,
 ) Tile {
-	tile := Tile{}
+	tile := Tile{
+		Name:         tt.Name,
+		TileCategory: TileCategoryNormal,
+		TileTypeID:   tt.ID,
+		TileCollisionType: placer.TileCollisionType,
+	}
 	updateOpts := TileUpdateOptions{
 		Neighbours: createOpts.Neighbours,
 		Tile:       &tile,
 	}
 	placer.UpdateTile(tt, updateOpts)
+
+
 	return tile
 }
 
 func (placer AutoPlacer) UpdateTile(
 	tt TileType, opts TileUpdateOptions,
 ) {
-	*opts.Tile = Tile{
-		SpriteID:     placer.sprite(opts.Neighbours),
-		Name:         tt.Name,
-		TileCategory: TileCategoryNormal,
-		TileTypeID:   tt.ID,
-		TileCollisionType: placer.TileCollisionType,
-	}
+	opts.Tile.SpriteID = placer.sprite(opts.Neighbours)
 }
 
 func (placer AutoPlacer) ItemSpriteID() render.SpriteID {
