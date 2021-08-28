9ed955f964e34ef08262a25f442868a4e7f943dd
Ted Brownlow <ted.brownlow@gmail.com>
2021-08-28 12:57:56 -0400

    add bit/int conversions

--- world/tile.go
+++ world/tile.go
@@ -33,6 +33,34 @@ func ConnectionsFromNeighbours(n tiling.DetailedNeighbours) Connections {
 	return Connections { Up: s.Up, Left: s.Left, Right: s.Right, Down: s.Down }
 }
 
+func composeBits(bits []bool) int {
+	place := 1
+	sum := 0
+	for _,bit := range bits {
+		if bit { sum += place }
+		place *= 2
+	}
+	return place
+}
+
+func decomposeBits(composed int, bits []bool) {
+	place := 2
+	for idx := range bits {
+		bits[idx] = composed%place == 1
+	}
+}
+
+// given some current connection state, cycles to another connection state.
+// loops over all possible states eventually
+func (c Connections) Next(valid Connections) Connections {
+	bits := []bool { c.Up, c.Left, c.Right, c.Down }
+	i := composeBits(bits)
+	decomposed := [4]bool{}
+	decomposeBits(i,decomposed[:])
+	d := decomposed
+	return Connections { d[0], d[1], d[2], d[3] }
+}
+
 type Tile struct {
 	SpriteID          render.SpriteID
 	TileCategory      TileCategory
