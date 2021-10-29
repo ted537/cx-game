9dc6ee13044cc7f343b85c7d205f6d8e0bea1371
Ted Brownlow <ted.brownlow@gmail.com>
2021-10-29 10:02:02 -0400

    refactor tiletype.go

--- world/tiletype.go
+++ world/tiletype.go
@@ -9,35 +9,6 @@ import (
 	"github.com/skycoin/cx-game/world/tiling"
 )
 
-type Placer interface {
-	CreateTile(TileType, TileCreationOptions) Tile
-	UpdateTile(TileType, TileUpdateOptions)
-	ItemSpriteID() render.SpriteID
-}
-
-// place tiles for a tiletype which has a single sprite
-type DirectPlacer struct {
-	SpriteID render.SpriteID
-	Tile     Tile
-}
-
-func (placer DirectPlacer) CreateTile(
-	tt TileType, opts TileCreationOptions,
-) Tile {
-	tile := placer.Tile
-	tile.SpriteID = placer.SpriteID
-	return tile
-}
-
-// nothing to update
-func (placer DirectPlacer) UpdateTile(
-	tt TileType, opts TileUpdateOptions) {
-}
-
-func (placer DirectPlacer) ItemSpriteID() render.SpriteID {
-	return placer.SpriteID
-}
-
 type TileTypeID uint32
 type TileType struct {
 	Name          string
